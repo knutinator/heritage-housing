52d80fc37b8a58b24001b0450c73ce53291db68d
knutinator <>
2023-05-21 13:14:41 +0000

    Add updated graph functions

--- app_pages/page_house_price_study.py
+++ app_pages/page_house_price_study.py
@@ -1,33 +1,33 @@
+from src.data_management import load_house_data
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
-from src.data_management import load_house_data
-import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("whitegrid")
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 def page_house_price_study_body():
-
     # load data
-    df = load_house_data
+    df = load_house_data()
 
     vars_to_study = ['GarageArea', 'GrLivArea',
                      'KitchenQual', 'OverallQual', 'YearBuilt']
 
     st.write("### House Price Study")
-        st.info(
-            f"* The client is interested in discovering how the house "
-            f"attributes correlate with the sale price. Therefore, the "
-            f"client expects data visualisations of the correlated "
-            f"variables against the sale price to show that."
-        
+
+    st.info(
+        f"* The client is interested in discovering how the house "
+        f"attributes correlate with the sale price. Therefore, the "
+        f"client expects data visualizations of the correlated "
+        f"variables against the sale price to show that."
+    )
+
     # inspect data
     if st.checkbox("Inspect House Attributes"):
         st.write(
             f"* The dataset has {df.shape[0]} rows and {df.shape[1]} columns, "
             f"find below the first 10 rows.")
-
         st.write(df.head(10))
 
     st.write("---")
@@ -42,36 +42,32 @@ def page_house_price_study_body():
     st.info(
         f"The correlation plots below indicate that the following are the "
         f"most important attributes that affect house sale prices: \n"
-
-        f"*Overall quality of the house \n"
-        f"*Size of ground floor \n"
-        f"*Quality of kitchen \n"
-        f"*Size of garage \n"
-        f"*Newness of house (year built)\n"
+        f"* Overall quality of the house \n"
+        f"* Size of ground floor \n"
+        f"* Quality of kitchen \n"
+        f"* Size of garage \n"
+        f"* Newness of house (year built)\n"
     )
 
 
+def corr_heatmap(df, vars_to_study):
+    vars_to_study.append('SalePrice')
+
+    # Plot the heat map
+    sns.heatmap(df[vars_to_study].corr(), annot=True, cmap="coolwarm")
+    plt.title("Correlation of features to SalePrice")
+    st.pyplot()
+
+    # Remove SalePrice from vars_to_study
+    vars_to_study.remove('SalePrice')
 
 
-# Select the top 5 variables with highest correlation with SalePrice
-corr = df.corr()
-vars_to_study.append('SalePrice')
-
-# Plot heat map
-sns.heatmap(df[top_corr_features].corr(), annot=True, cmap="coolwarm")
-plt.title("Correlation of features to SalePrice")
-plt.show()
-
-
-
-# select the top 5 variables that are most strongly correlated with SalePrice
-top_5_vars = df[['GarageArea', 'GrLivArea', 'KitchenQual', 'OverallQual', 'YearBuilt']]
-
-# create scatterplots showing the relationship between each variable and SalePrice
-for var in top_5_vars:
-    plt.figure()
-    plt.scatter(df[var], df['SalePrice'], alpha=0.5)
-    plt.xlabel(var)
-    plt.ylabel('SalePrice')
-    plt.title(f'{var} vs. SalePrice')
-    plt.show()
+def corr_scatterplot(df, vars_to_study):
+    # create scatterplots showing the relationship between each variable and SalePrice
+    for var in vars_to_study:
+        plt.figure()
+        plt.scatter(df[var], df['SalePrice'], alpha=0.5)
+        plt.xlabel(var)
+        plt.ylabel('SalePrice')
+        plt.title(f'{var} vs. SalePrice')
+        st.pyplot()
