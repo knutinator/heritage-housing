f45cbf98ac571de5bbc2f0cc281c46ae6583c903
knutinator <>
2023-05-21 13:17:02 +0000

    Fix long lines in app.py

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ from app_pages.page_house_price_study import page_house_price_study_body
 # from app_pages.page_predict_price import page_predict_price_body
 # from app_pages.page_technical_details import page_technical_details_body
 
-app = MultiPage(app_name= "House Price Predictor") # Create an instance of the app 
+app = MultiPage(app_name="House Price Predictor") 
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("House Price Study", page_house_price_study_body)
-# app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+# app.add_page("Project Hypothesis and Validation",
+# page_project_hypothesis_body)
 # app.add_page("ML: Predict Price", page_predict_price_body)
 # app.add_page("ML: Technical Details", page_technical_details_body)
 
-app.run() # Run the  app
+app.run()  # Run the app
