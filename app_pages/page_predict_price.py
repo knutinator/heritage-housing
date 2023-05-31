6d05df8cdd377adee49d91812247966448b6bcbf
knutinator <>
2023-05-31 08:03:57 +0000

    Add price prediction function

--- app_pages/page_predict_price.py
+++ app_pages/page_predict_price.py
@@ -1,18 +1,27 @@
+from src.data_management import load_house_data, load_pkl_file
 import streamlit as st
 import numpy as np
 import pandas as pd
-from src.data_management import load_house_price_data, load_pkl_file
 
 
-def page_predict_house_price_body():
+def page_predict_price_body():
 #load needed files
 
     version = 'v1'
     pipeline = load_pkl_file(
-        f"outputs/predict_price/{version}/best_regressor_pipeline.pkl")
-    best_features = (pd.read_csv(f"outputs/predict_price/{version}/X_train.csv")
+        f"outputs/predict_house_price/{version}/best_regressor_pipeline.pkl")
+    best_features = (pd.read_csv(f"outputs/predict_house_price/{version}/X_train.csv")
                      .columns
                      .to_list()
                      )
 
     df = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
+
+#run price prediction on inherited houses using trained ML pipeline
+    
+    st.write(df.head())
+    df = df.filter(best_features)
+    price_prediction = pipeline.predict(df).round(0)
+    df['Predicted House Price'] = price_prediction
+    
+    st.write(df.head())
