ca0fe3f70852ad019c1b0da127349086fcba5bb6
knutinator <>
2023-05-31 12:11:56 +0000

    Add imports and intro to technical details page

--- app_pages/page_technical_details.py
+++ app_pages/page_technical_details.py
@@ -1,4 +1,30 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.data_management import load_house_data, load_pkl_file
+
 
 
 def page_technical_details_body():
+
+ # load house price pipeline files
+    version = 'v1'
+    pipeline = load_pkl_file(
+        f"outputs/predict_house_price/{version}/best_regressor_pipeline.pkl")
+    house_price_feat_importance = plt.imread(
+        f"outputs/predict_house_price/{version}/features_importance.png")
+    X_train = pd.read_csv(f"outputs/predict_house_price/{version}/X_train.csv")
+    X_test = pd.read_csv(f"outputs/predict_house_price/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/predict_house_price/{version}/y_train.csv").squeeze()
+    y_test = pd.read_csv(
+        f"outputs/predict_house_price/{version}/y_test.csv").squeeze()
+
+    st.write("### Machine Learning house price pipeline details")
+    st.info(
+        f"* The goal of the project was a model and a pipeline with an R2 "
+        f"score of at least 0.75 on both train and test sets. \n"
+        f"* The pipeline achieves XX on the train set and XX on the test set. \n"
+    )
+
+    
