40cb80c2bd5dacb3aac362c02bb48c13d6e91567
knutinator <>
2023-05-31 09:58:34 +0000

    Add descriptions to price predictor page

--- app_pages/page_predict_price.py
+++ app_pages/page_predict_price.py
@@ -18,10 +18,21 @@ def page_predict_price_body():
     df = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
 
 #run price prediction on inherited houses using trained ML pipeline
-    
+    st.write("### Predicted sale prices of the client's four inherited houses")
+    st.write(
+        f"* These are the characteristics of the clients houses (scroll to view more):"
+    )
     st.write(df.head())
     df = df.filter(best_features)
     price_prediction = pipeline.predict(df).round(0)
     df['Predicted House Price'] = price_prediction
-    
+    st.write(
+        f"\n"
+    )
+    st.write(
+        f"* Here are the predicted sale prices of the clients houses, "
+        f"together with the attributes that were used in our machine "
+        f"learning model to produce the prediction:"
+
+    )
     st.write(df.head())
